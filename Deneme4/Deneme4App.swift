bc9a44690a8be72f23fd623370fcfecc6c90eef5
ErenUnal24 <bilalunal24@gmail.com>
2024-05-22 19:02:01 +0300

    Initial Commit

---
+++ Deneme4/Deneme4App.swift
@@ -0,0 +1,17 @@
+//
+//  Deneme4App.swift
+//  Deneme4
+//
+//  Created by Eren on 22.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Deneme4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Deneme4UITests/Deneme4UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Deneme4UITestsLaunchTests.swift
+//  Deneme4UITests
+//
+//  Created by Eren on 22.05.2024.
+//
+
+import XCTest
+
+final class Deneme4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
